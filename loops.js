b6a22e9225a22e79d317a3b407ace6b3b6ed7a07
Austin Buhler <austinbuhler@gmail.com>
2020-05-08 16:32:35 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,12 +1,13 @@
 function forLoop(array) {
   for (let i = 0, i < 25; i++) {
-    if (i = 0) {
-      console.log(`I am ${i} strange loop.`)
+    if (i === 1) {
+      array[i] = "I am 1 strange loop."
     }
     else {
-      console.log(`I am ${i} strange loops.`)
+      array[i] = `I am ${i} strange loops.`
     }
   }
+  console.log(array);
+  return array;
 }
 
-forLoop()
